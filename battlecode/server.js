eee7a52063c86072992acdfc65aa178f10a87e0f
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-15 13:55:46 -0800

    Insert http dependency, initialize socket instance on server and client (not working on client)

--- battlecode/server.js
+++ battlecode/server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 require(__dirname + '/serverConfig/middleware.js')(app, express);
 require(__dirname + '/serverConfig/routes.js')(app, express);
 
+io.on('connection', function(socket) {
+	console.log('a user connected');
+})
+
 app.listen(3000);
+
+console.log('server listening on port 3000');
 
 module.exports = app;
