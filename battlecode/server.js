63167cbae63f2c41f046904cc98dce5df31988aa
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-15 12:54:46 -0800

    Add morgan dep, comment out routing in route.js, add middleware file

--- battlecode/server.js
+++ battlecode/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 require('./serverConfig/middleware.js')(app, express);
-require('./serverConfig/routes.js')(app, express);
+//require('./serverConfig/routes.js')(app, express);
 
 app.listen(3000);
 
