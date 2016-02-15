37408dbefa8d4ff52b34a1c1182a4e83a6292412
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-15 12:30:17 -0800

    Add server file, which will use yet to be created middlewar

---
+++ battlecode/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+require('./serverConfig/middleware.js')(app, express);
+require('./serverConfig/routes.js')(app, express);
+
+app.listen(3000);
+
+module.exports = app;
