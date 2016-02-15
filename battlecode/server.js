0fa3c3ef88b444a1c673ccea0f442520df4d369d
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-15 14:14:57 -0800

    Replace depedency on client side (sockeit.io), initialize server to listen on socket

--- battlecode/server.js
+++ battlecode/server.js
@@ -4,15 +4,14 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+
 require(__dirname + '/serverConfig/middleware.js')(app, express);
 require(__dirname + '/serverConfig/routes.js')(app, express);
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
-})
+});
 
-app.listen(3000);
-
-console.log('server listening on port 3000');
-
-module.exports = app;
+server.listen(3000, function() {
+	console.log('listening on 3000');
+});
