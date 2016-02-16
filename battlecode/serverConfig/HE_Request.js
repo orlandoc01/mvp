b2794b21cc9cc3c1a7950354b8574ac7d44ea968
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-15 18:28:18 -0800

    Add HackerEarth dependency, modify HE_Request, add first testCode room

--- battlecode/serverConfig/HE_Request.js
+++ battlecode/serverConfig/HE_Request.js
@@ -1,19 +1,25 @@
-var CLIENT_SECRET = '65b5102503d48ce78eaccc72a26186f815da491f';
-var CLIENT_ID = '0dd3ce63abb7e569b3e1a6181e40b837b89eaab04fc6.api.hackerearth.com';
-var RUN_URL = 'http://api.hackerearth.com/code/run/';
+var HackerEarth = require('hackerearth-node');
 var request = require('request');
+
+var CLIENT_SECRET = 'd246c10356414930c48fd6d7e4ea09ec0d8398b5';
+var CLIENT_ID = 'a909c9e433318fc9cd5ff8c291fb2c449c0149930e04.api.hackerearth.com';
+var RUN_URL = 'http://api.hackerearth.com/v3/code/run/';
+
+
 
 module.exports.runCode = function(code, lang) {
 	console.log('secret is ' + CLIENT_SECRET);
 	var data = {
-		'client_id': CLIENT_ID,
+		//'client_id': CLIENT_ID,
 		'client_secret': CLIENT_SECRET,
-		'async': 1,
+		'async': 0,
 		'source': code,
 		'lang': lang || 'JAVASCRIPT',
 		'time_limit': 5,
 		'memory_limit': 262144,
 	};
+
+
 
 	request.post(RUN_URL, data, function(error1, success) {
 		console.log('got a response');
