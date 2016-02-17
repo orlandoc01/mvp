917dce4f841d5458715a266e9721e21f67000ab4
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-16 16:56:39 -0800

    Refactor with EJS

--- battlecode/serverConfig/middleware.js
+++ battlecode/serverConfig/middleware.js
@@ -6,5 +6,7 @@ module.exports = function(app, express) {
 	app.use(morgan('dev'));
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
-	app.use(express.static(__dirname + '/../client'));
+	app.set('view engine', 'ejs');
+	app.set('views', __dirname + '/../views');
+	app.use(express.static(__dirname + '/../public'))
 }
